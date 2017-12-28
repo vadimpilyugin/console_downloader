300d91281327ffcbe03b29c5215b5fa3a019a895
vadim <vadimpilyugin@gmail.com>
2017-12-28 05:34:04 +0300

    added Gemfile, config

--- init.rb
+++ init.rb
@@ -1,23 +1,33 @@
-require_relative 'printer'
-require_relative 'interface'
-require_relative 'menu'
-
 require 'irb'
-input_obj = Input.new(io:$stdin)
-query_obj = QueryObject.new
-sr_obj = SearchResultsObject.new(query_obj)
-descr_obj = DescrObject.new(sr_obj)
-cursor = Cursor.get
-finalizer = FinalizerObject.new
+require 'yaml'
+$config = YAML.load_file('config.yml')
 
-menu = Menu.new(
-	input_obj:input_obj,
-	query_obj:query_obj, 
-	sr_obj:sr_obj,
-	descr_obj:descr_obj,
-	finalizer:finalizer
-)
+require_relative 'src/printer'
+require_relative 'src/interface'
+require_relative 'src/menu'
+
+
 begin
+	cursor = Cursor.get
+	input_obj = Input.new(io:$stdin)
+	
+	Printer::assert(
+		expr:Dir.exists?($config['torrents_dir']),
+		msg:"torrents_dir does not exist!"
+	)
+
+	query_obj = QueryObject.new
+	sr_obj = SearchResultsObject.new(query_obj)
+	descr_obj = DescrObject.new(sr_obj)
+	finalizer = FinalizerObject.new
+
+	menu = Menu.new(
+		input_obj:input_obj,
+		query_obj:query_obj, 
+		sr_obj:sr_obj,
+		descr_obj:descr_obj,
+		finalizer:finalizer
+	)
 	loop {
 		menu.main_loop
 	}
